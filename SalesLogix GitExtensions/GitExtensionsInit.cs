b6ce2b379a64ecde5c282b7411e07563a2f27ad1
Ryan Farley <ryan@farleyzone.com>
2009-10-13 01:05:18 -0700

    Initial commit

---
+++ SalesLogix GitExtensions/GitExtensionsInit.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+using System.IO;
+using log4net;
+using Sage.Platform;
+using Sage.Platform.Application;
+using Sage.Platform.Application.UI;
+using Sage.Platform.Projects;
+using Sage.Platform.Projects.Interfaces;
+
+namespace FX.SalesLogix.Modules.GitExtensions
+{
+    public class GitExtensionsInit : ModuleInit<UIWorkItem>, IModuleConfigurationProvider
+    {
+        public const string AdminWorkItemID = "9D4BB197-6FBC-4bf4-AAF5-7EE958603EDE";
+
+        private IProjectContextService _projectContextService;
+        private static readonly ILog _log = LogManager.GetLogger("FX.Modules");
+
+        public override string ToString()
+        {
+            return GitExtensionResources.ModuleName;
+        }
+
+        public ModuleConfiguration GetConfiguration()
+        {
+            return ModuleConfiguration.LoadFromResource("FX.SalesLogix.Modules.GitExtensions.FX.SalesLogix.Modules.GitExtensions.Configuration.xml", base.GetType().Assembly);
+        }
+
+        [CommandHandler("cmd://GitExtensionsModule/GitCommit")]
+        public void GitCommit_Click(object sender, EventArgs e)
+        {
+            ProjectWorkspace workspace = _projectContextService.ActiveProject.ProjectWorkspace;
+            MessageBox.Show("Workspace Type: " + (workspace.WorkingPath.ToLower().StartsWith("vfs") ? "VFS" : "File System") + "\r\nName: " + workspace.Name + "\r\nPath: " + workspace.WorkingPath);
+        }
+
+        [CommandHandler("cmd://GitExtensionsModule/GitPull")]
+        public void GitPull_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pull");
+        }
+
+        [CommandHandler("cmd://GitExtensionsModule/GitPush")]
+        public void GitPush_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Push");
+        }
+
+        [CommandHandler("cmd://GitExtensionsModule/GitSettings")]
+        public void GitSettings_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Settings");
+        }
+
+        [CommandHandler("cmd://GitExtensionsModule/GitExtensions")]
+        public void GitExtensions_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Launch Git Extensions");
+        }
+
+        [ServiceDependency]
+        public IProjectContextService ProjectContextService
+        {
+            get
+            {
+                return this._projectContextService;
+            }
+            set
+            {
+                this._projectContextService = value;
+            }
+        }
+
+        protected override void Configure(XmlElement xmlConfig)
+        {
+        }
+
+        protected override void Load()
+        {
+            _log.Info("Loading Git Extensions for SalesLogix");
+        }
+    }
+}
---
+++ SalesLogix GitExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SalesLogix Git Extensions")]
+[assembly: AssemblyDescription("Git Extensions for SalesLogix")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Customer FX Corporation")]
+[assembly: AssemblyProduct("SalesLogix Git Extensions")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e3af0753-df3b-4c73-a26a-5c8af450ffbd")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
