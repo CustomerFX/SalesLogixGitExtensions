a14cee05d60ae5686ae863a22c1264efd17f3470
Ryan Farley <ryan@farleyzone.com>
2010-11-30 16:26:54 -0700

    Added branch object into develop branch

---
+++ SalesLogix GitExtensions/Model/Branch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FX.SalesLogix.Modules.GitExtensions.Model
+{
+	public class Branch
+	{
+		public Branch()
+		{
+			this.Current = false;
+		}
+
+		public Branch(string Name) : this()
+		{
+			this.Name = Name;
+		}
+
+		public Branch(string Name, bool Current)
+		{
+			this.Name = Name;
+			this.Current = Current;
+		}
+
+		public string Name { get; set; }
+		public bool Current { get; set; }
+
+		public override string ToString()
+		{
+			return this.Name;
+		}
+	}
+}
